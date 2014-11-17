04559d7f2b8b28e8ef0db8ae7db2b6debf997de1
li ouyang <li.ouyang@gmail.com>
2014-11-17 00:31:34 -0800

    change email address

--- app/mailers/waiver_mailer.rb
+++ app/mailers/waiver_mailer.rb
@@ -3,7 +3,7 @@ class WaiverMailer < ActionMailer::Base
   def waiver_confirmation(waiver)
     @waiver = waiver
     attachments["signature.png"] = File.read("#{Rails.root}/app/assets/images/signature.png")
-    mail(:to => "lauren@iamyoustudio.com",
+    mail(:to => "waivers@iamyoustudio.com",
             :subject => "#{Time.now.strftime('%m/%d/%y')} #{waiver.full_name} has signed a waiver",
             :from => "li.ouyang@gmail.com")
   end
